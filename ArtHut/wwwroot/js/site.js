a9607bfe37fd15483c05eda38a7eb5c0e55fbfcc
Veli Ashikov <181veli@gmail.com>
2023-01-14 21:53:07 +0200

    Laout 2.0 & index 1.0 first out

--- ArtHut/wwwroot/js/site.js
+++ ArtHut/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
 
-// Write your JavaScript code.
+
+let PresentationslideIndex = 1;
+PresentationSlides(PresentationslideIndex);
+
+function plusPresentationSlides(n) {
+    PresentationSlides(PresentationslideIndex += n);
+}
+
+function currentPresentationSlides(n) {
+    PresentationSlides(PresentationslideIndex = n);
+}
+function PresentationSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("MyPresentationSlides");
+    let dots = document.getElementsByClassName("Slide");
+    if (n > slides.length) { PresentationslideIndex = 1 }
+    if (n < 1) { PresentationslideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[PresentationslideIndex - 1].style.display = "block";
+    dots[PresentationslideIndex - 1].className += " active";
+}
